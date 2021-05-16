736c954836b305d89b1d9533b47982e9d55b222f
Kryspin Trawnik <ktrawnik@gmail.com>
2021-05-16 22:05:02 +0100

    ViewModels

--- CafeMVC.Application/ViewModels/Orders/OrderViewForVM.cs
+++ CafeMVC.Application/ViewModels/Orders/OrderViewForVM.cs
@@ -1,15 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace CafeMVC.Web.Models
 {
     public class OrderViewForVM
     {
-        public List<MenuForListVM> MenuList {get; set;}
+        public int Id { get; set; }
 
+        public string CustomerName { get; set; }
 
-        public OrderViewForVM()
-        {
-            MenuList = new List<MenuForListVM>();
-        }
+        public double TotalPrice { get; set; }
+
+        public List<ProductForListVM> OrderedProduct { get; set; }
+
+        public DateTime DateOfOrder { get; set; }
+
+        public string DeliveryAddress { get; set; }
+
     }
 }

fa7ac62e51a01e40181f20c1dab551f218df4fbf
Kryspin Trawnik <ktrawnik@gmail.com>
2021-05-18 07:35:48 +0100

    ViewModelsDevelopment

--- CafeMVC.Application/ViewModels/Products/ProductForListVM.cs
+++ CafeMVC.Application/ViewModels/Products/ProductForListVM.cs
@@ -1,6 +1,17 @@
-﻿namespace CafeMVC.Web.Models
+﻿using System.Collections.Generic;
+
+namespace CafeMVC.Web.Models
 {
     public class ProductForListVm
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public List<IngredientForVm> Ingredients { get; set; }
+
+        public DietInfoForVM DietInformation { get; set; }
     }
 }

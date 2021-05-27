7c8802660b3cb84e689249305b5065395bf4adb9
Kryspin Trawnik <ktrawnik@gmail.com>
2021-05-27 07:11:35 +0100

    Automapping

--- CafeMVC.Application/ViewModels/Customer/AddressForListVm.cs
+++ CafeMVC.Application/ViewModels/Customer/AddressForListVm.cs
@@ -1,9 +1,13 @@
-﻿namespace CafeMVC.Application.ViewModels.Customer
+﻿using CafeMVC.Application.Interfaces.Mapping;
+
+namespace CafeMVC.Application.ViewModels.Customer
 {
-    public class AddressForListVm
+    public class AddressForListVm : IMapFrom<CafeMVC.Domain.Model.Address>
     {
         public int Id { get; set; }
 
         public string Type { get; set; }
+
     }
+
 }

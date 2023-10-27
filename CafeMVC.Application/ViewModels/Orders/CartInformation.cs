f6845e937420ee99d779d635a9b043ef7b5a1e62
KryspinTrawnik <ktrawnik@gmail.com>
2023-10-27 07:01:07 +0100

    UserCartOptions

--- CafeMVC.Application/ViewModels/Orders/CartInformation.cs
+++ CafeMVC.Application/ViewModels/Orders/CartInformation.cs
@@ -1,19 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CafeMVC.Application.ViewModels.Orders
+﻿namespace CafeMVC.Application.ViewModels.Orders
 {
     public class CartInformation
     {
-        public bool IsCollection  { get; set; }
+        public bool IsCollection { get; set; }
 
         public int PaymentTypeId { get; set; }
 
         public string PaymentName { get; set; }
 
         public string Postcode { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public int AddressId { get; set; }
     }
 }
